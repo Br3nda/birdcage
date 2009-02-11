1321ae68d367514b1ba2f5f9bb779f5455a80d71
Brenda Wallace <shiny@cpan.org>
2009-02-12 12:09:29 +1300

    updated from http://www.unpunk.com/~greck/shoebox/birdcage.txt

--- birdcage.pl
+++ birdcage.pl
@@ -1,8 +1,33 @@
 #!/usr/bin/perl
 #
-# birdcage.pl 0.2.1
-# Copyright (C) 2007 Greck Cannon
-# greck@unpunk.com
+# birdcage.pl 0.2.2
+#
+# Copyright (C) 2009 Greck Cannon
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Greck Cannon nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY GRECK CANNON ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL GRECK CANNON BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+###############################################################################
 #
 # This script is designed to dump a day's worth of Tweets into your
 # LiveJournal.  The API is a bit weird, and only allows you to reach
@@ -24,6 +49,9 @@
 #
 # CHANGELOG
 #
+# 0.2.2
+# - Added BSD license notification.
+#
 # 0.2.1
 # - Added max 3 retries when logging in to LJ and again when posting.
 #
@@ -35,28 +63,25 @@
 # - Initial release.
 #
 
-
-my $result = GetOptions('help' => \$help, 'username=s' => \$username, 'password=s' => \$password, 'sync' => \$sync);
-
 #
 # TWITTER ACCOUNT INFORMATION
 #
-$twitter_user = '';
-$twitter_pass = '';
+$twitter_user = "user";
+$twitter_pass = "pass";
 
 #
 # LIVEJOURNAL ACCOUNT INFORMATION
 #
-$lj_user = ''; 
-$lj_pass = '';
+$lj_user = "user";
+$lj_pass = "pass";
 #
 # public, friends, private
 #
-$lj_sec = "friends";
+$lj_sec = "public";
 #
 # tags
 #
-@lj_tags = ( "twitter" );
+#@lj_tags = ( "twitter" );
 #
 # userpic (use "keywords" value)
 #
@@ -77,91 +102,105 @@ use POSIX qw(strftime);
 use LWP::Simple;
 use Date::Manip;
 use LJ::Simple;
-use Net::Twitter;
 
 &Date_Init("TZ=$tz");
 $midnight_today = UnixDate(scalar(localtime),"%a,+%d+%b+00:00:00+%Z");
-my $twit = Net::Twitter->new( username => $twitter_user, password => $twitter_pass);
-$timeline = $twit->user_timeline({id => $twitter_user});
+
+for ( $i = 0; $i < 3; ++$i ) {
+
+    if ( $tweets_xml = get("http://$twitter_user:$twitter_pass\@twitter.com/statuses/user_timeline/$twitter_user.xml?count=20&since=$midnight_today") ) {
+        last;
+} else {
+        sleep 5;
+}
+
+}
+
+if ( not defined($tweets_xml) ) {
+    die "$0: Twitter request failed";
+}
+
+@tweets = split '\n', $tweets_xml;
 
 #
 # I know, there's a real XML parser I should be using...
 #
 
-#while ( $_ = shift @tweets ) {
+while ( $_ = shift @tweets ) {
 
-
-$i = 0;
-my @entries;
-foreach my $tweet (@{ $timeline }) {
-	#next if $tweet->{'text'} =~ /http:/;
-	next if $tweet->{'text'} =~ /\@[A-Z]/i;
-	next if (!$tweet->{'text'});
-	$entries[$i] = "<li>" . $tweet->{'text'} ."</li>\n";
-	$i++;
+    if ( /<created_at>(.+)<\/created_at>/ ) {
+    
+        $tstamp = UnixDate(ParseDate($1),"%H:%M:%S");
+    
 }
-@entries = reverse(@entries);
 
-$entry = '<ul>' . "\n";
-foreach my $line (@entries) {
-  $entry .= $line;
+    if ( /<text>(.*)<\/text>/ ) {
+    
+        unshift @entry, "<TR><TD VALIGN=\"top\"><CODE>$tstamp</CODE></TD><TD VALIGN=\"top\">&nbsp;&mdash;&nbsp;</TD><TD VALIGN=\"top\">$1</TD></TR>";
+    
 }
-$entry .= '</ul>' . "\n";
+
+}
+
+$entry = '<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0">' . "\n";
+
+foreach $_ ( @entry ) { $entry .= "$_\n"; }
+
+$entry .= '</TABLE>' . "\n";
 
 for ( $i = 0; $i < 3; ++$i ) {
 
-	if ( $lj = new LJ::Simple ( { user => $lj_user, pass => $lj_pass } ) ) {
-		last;
-	} else {
-		sleep 5;
-	}
+    if ( $lj = new LJ::Simple ( { user => $lj_user, pass => $lj_pass } ) ) {
+        last;
+} else {
+        sleep 5;
+}
 
 }
 
-
 if ( not defined($lj) ) {
-	die "$0: Failed to login to LiveJournal ($LJ::Simple::error)";
+    die "$0: Failed to login to LiveJournal ($LJ::Simple::error)";
 }
 
 %lj_event=();
 
 $lj->NewEntry(\%lj_event)
-	or die "$0: Failed to create new entry ($LJ::Simple::error)";
+    or die "$0: Failed to create new entry ($LJ::Simple::error)";
 
 $lj->SetProtect(\%lj_event,$lj_sec)
-	or die "$0: Failed to set protection ($LJ::Simple::error)";
+    or die "$0: Failed to set protection ($LJ::Simple::error)";
 
 $lj->SetEntry(\%lj_event,$entry)
-	or die "$0: Failed to set entry ($LJ::Simple::error)";
+    or die "$0: Failed to set entry ($LJ::Simple::error)";
 
-$lj->SetSubject(\%lj_event,"Todays' Twitter rants")
-	or die "$0: Failed to set subject ($LJ::Simple::error)";
+$lj->SetSubject(\%lj_event,"Twitter, digested by birdcage.pl")
+    or die "$0: Failed to set subject ($LJ::Simple::error)";
 
 if ( defined @lj_tags ) {
-	$lj->Setprop_taglist(\%lj_event,@lj_tags)
-		or die "$0: Failed to set tags ($LJ::Simple::error)";
+    $lj->Setprop_taglist(\%lj_event,@lj_tags)
+        or die "$0: Failed to set tags ($LJ::Simple::error)";
 }
 
 $lj->Setprop_preformatted(\%lj_event,1)
-	or die "$0: Failed to set formatting ($LJ::Simple::error)";
+    or die "$0: Failed to set formatting ($LJ::Simple::error)";
 
 if ( defined $lj_userpic ) {
-	$lj->Setprop_picture_keyword(\%lj_event,$lj_userpic)
-		or die "$0: Failed to set userpic ($LJ::Simple::error)";
+    $lj->Setprop_picture_keyword(\%lj_event,$lj_userpic)
+        or die "$0: Failed to set userpic ($LJ::Simple::error)";
 }
 
 for ( $i = 0; $i < 3; ++$i ) {
 
-	( $lj_item_id, $lj_anum, $lj_html_id )=$lj->PostEntry(\%lj_event);
+    ( $lj_item_id, $lj_anum, $lj_html_id )=$lj->PostEntry(\%lj_event);
 
-	if ( defined($lj_item_id) ) {
-		last;
-	} else {
-		sleep 5;
-	}
+    if ( defined($lj_item_id) ) {
+        last;
+} else {
+        sleep 5;
+}
 
 }
 
 if ( not defined($lj_item_id) ) {
-	die "$0: Failed to post entry ($LJ::Simple::error)";
+    die "$0: Failed to post entry ($LJ::Simple::error)";
 }
